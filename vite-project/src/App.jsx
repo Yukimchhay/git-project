4720ff32492cf5a24f59a9501ba788a2ebec9ea4
Nacha <nacha@Nachas-MacBook-Pro.local>
2025-01-19 16:07:11 +0700

    first is allow

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
     <Header></Header>
+    <Header></Header>
       <h1>hello world</h1>   
       <h2>hello hurhuifeh</h2>
       <p>HI my name is kimchay i am your teammate and i will take this team to be pro and become a full stack teammate not enter allow week anymore</p>
