6b329ffc1365c5049a4f426d3fa6079b556f1003
Nacha <nacha@Nachas-MacBook-Pro.local>
2025-01-19 15:33:18 +0700

    finish

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>hello world</h1>
+      <h2>hello hurhuifeh</h2>
+      <header>hI man dhfrhsiuefhdhcjkshdhsdfjwiuehfcnihewuhfnsdbcv dfo ghk i have been faster than you not ou vacn </header>
+      <p>HI my name is kimchay i am your teammate and i will take this team to be pro and become a full stack teammate not enter allow week anymore</p>
     </>
   )
 }
