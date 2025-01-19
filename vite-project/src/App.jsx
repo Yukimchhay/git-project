61eab843f42a67ec42bf4c415b3ac6f1bd7f8bfd
Nacha <nacha@Nachas-MacBook-Pro.local>
2025-01-19 16:04:10 +0700

    new

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
+import Header from './header'
 
 function App() {
 
   return (
     <>
-      <h1>hello world</h1>
+    <Header></Header>
+      <h1>hello world</h1>   
       <h2>hello hurhuifeh</h2>
-      <header>hI man dhfrhsiuefhdhcjkshdhsdfjwiuehfcnihewuhfnsdbcv dfo ghk i have been faster than you not ou vacn </header>
       <p>HI my name is kimchay i am your teammate and i will take this team to be pro and become a full stack teammate not enter allow week anymore</p>
     </>
   )
---
+++ vite-project/src/header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <header>
+      <h1>Header to the best top of the most header full </h1>
+    </header>
+  );
+}export default Header;
